129ff1109855194150a255d3ed0def13002e2749
shi3719 <shi3719@naver.com>
2024-03-18 01:49:43 +0900

    3차 수정

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,10 +6,99 @@ document.documentElement.style.setProperty('--vh', `${vh}px`);
 window.addEventListener('resize', () => {
     let vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty('--vh', `${vh}px`);
+    ingredientNextHeightSelect();
 })
 
-const numInputWraps = document.querySelectorAll('.num_input_wrap');
+// ingredient_wrapper 영역설정
+const ingredientWrapper = document.querySelector('.ingredient_wrapper');
+const ingredientNext = document.querySelector('.ingredient_next');
 
+const ingredientNextHeightSelect = () => {
+    const ingredientWrapperHeight = ingredientWrapper.offsetHeight;
+    ingredientNext.style.height = `${ingredientWrapperHeight}px`
+}
+
+ingredientNextHeightSelect();
+
+//재료 표시 영역 ingredient_wrap 내용 없으면 감추기
+const ingredientWraps = document.querySelectorAll('.ingredient_wrap');
+const ingredientWrapChange = () => {
+    ingredientWraps.forEach((ingredientWrap) => {
+        const ingredients = Array.from(ingredientWrap.children[1].children);
+        ingredients.forEach((ingredient) => {
+            if(ingredient.classList.contains('on')){
+                ingredient.parentElement.parentElement.classList.add('on');
+            }else{
+                ingredient.parentElement.parentElement.classList.remove('on');
+            }
+        })
+    })
+
+    ingredientNextHeightSelect();
+}
+
+const values = document.querySelectorAll('.value');
+
+// 재료 표시
+
+const drawValue = () => {
+
+
+    ingredientWrapChange();
+};
+
+//재료 1차 표시 
+const valueChange = (item, amount, num) => {
+    values.forEach((valueHtml) => {
+        if(valueHtml.classList.contains(item)){
+            if(amount == 0){
+                valueHtml.parentElement.parentElement.classList.remove('on');
+            }else{
+                if(valueHtml.innerHTML == ''){
+                    valueHtml.parentElement.parentElement.classList.add('on');
+                    valueHtml.innerHTML = amount;
+                }else{
+                
+                }
+            }
+        }
+    })
+}
+
+
+// 물품 수량 변경
+const quantityChange = (input) => {
+    const item = input.classList[1];
+    const amountHtml = input.classList[2];
+    const amount = Number(amountHtml.slice(0, -1));
+    const quantity = input.value;
+    const totalQuantity = amount * quantity;
+
+    if(item == 'white_candle'){
+        const pillawaxAmount = totalQuantity * 0.9;
+        valueChange('pillawax', pillawaxAmount, amount)
+        const bambooAmount = totalQuantity * 0.065;
+        const mugwortAmount = totalQuantity * 0.035;
+    }else if(item == 'green_candle'){
+        const pillawaxAmount = totalQuantity * 0.9;
+        const gardenpartyAmount = totalQuantity * 0.075;
+        const mugwortAmount = totalQuantity * 0.025;
+        const huntergreenAmount = pillawaxAmount * 0.00051;
+        const magentaAmount = pillawaxAmount * 0.000114;
+        const canaryyellowAmount = pillawaxAmount * 0.00017;
+    }else if(item == 'red_candle'){
+
+    }else if(item == 'black_candle'){
+
+    }else if(item == 'white_mika'){
+
+    }else if(item == 'black_mika'){
+
+    }
+}
+
+// 물품 수량 제어
+const numInputWraps = document.querySelectorAll('.num_input_wrap');
 const inputNumZero = (inputNum) => {
     const inputNumber = inputNum.value;
     if(inputNumber < 1){
@@ -19,6 +108,8 @@ const inputNumZero = (inputNum) => {
         inputNum.style.opacity = "100%";
         inputNum.previousElementSibling.style.opacity = "100%";
     }
+
+    quantityChange(inputNum);
 }
 
 numInputWraps.forEach((numInputWrap) => {
@@ -55,11 +146,3 @@ objectThemes.forEach((objectTheme) => {
         objectTheme.nextElementSibling.classList.toggle('hide');
     })
 })
-
-// ingredient_wrapper 영역설정
-const ingredientWrapper = document.querySelector('.ingredient_wrapper');
-const ingredientNext = document.querySelector('.ingredient_next');
-
-c
-
-
